e3a560a9f0778d80985b209305a91a2dce510c24
rishabh27892 <rishabh.chauhan27892@gmail.com>
2019-01-31 16:15:52 -0500

    code problem

---
+++ function.py
@@ -0,0 +1,67 @@
+import datetime
+import calendar
+mm = 0
+dd = 0
+yyyy = 0
+
+def parsfun(date):
+    fail = False
+    postdate = date.split(".")
+    dd = int(postdate[0])
+    mm = int(postdate[1])
+    yyyy = int(postdate[2])
+    if dd < 1 or dd > 31:
+        print ("invalid date")
+        fail = True
+    if mm < 1 or mm > 12:
+        print ("invalid month")
+        fail = True
+    if yyyy < 1 or len(postdate[2]) < 4:
+        print ("invalid year")
+        fail = True
+    if fail is True:
+        print ("try again with valid dates")
+        exit()
+    return [dd, mm, yyyy]
+
+def leaplib(year):
+    return calendar.isleap(year)
+
+def leapfun(year):
+    if year%4 == 0 and year%100!=0:
+        return True
+    elif year%400 == 0:
+        return True
+    else:
+        return False
+
+def weeklyprice(date):
+    if 0 < date < 8:
+        price = 0.05
+    elif 7 < date < 15:
+        price = 0.10
+    elif 14 < date < 22:
+        price = 0.15
+    elif 21 < date < 29:
+        price = 0.20
+    elif 28 < date < 32:
+        price = 0.10
+    print (price)
+    return price
+
+def final_dates(date):
+    dates = parsfun(date)
+    condition = leapfun(dates[2])
+    if dates[1] == 2:
+        if condition is False and int(dates[0]) > 28:
+            print ("Since it is not a leap year, Feb must have only 28 days")
+            exit()
+        elif condition is True and int(dates[0]) > 29:
+            print ("Since it is a leap year, Feb must have only 29 days")
+            exit()
+    return [dates[0], dates[1], dates[2]]
+
+def check_day(date):
+    # format is yyyy, mm, dd
+    day = datetime.date(date[2], date[1], date[0]).weekday()
+    return day
---
+++ main.py
@@ -0,0 +1,50 @@
+import sys
+from function import *
+month_dict = {
+"jan" : 31,
+"feb" : 28,
+"mar" : 31,
+"apr" : 30,
+"may" : 31,
+"jun" : 30,
+"jul" : 31,
+"aug" : 31,
+"sep" : 30,
+"oct" : 31,
+"nov" : 30,
+"dec" : 31
+}
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekday = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday', 'sunday']
+weeklyPrice = {
+    "first" : 0.05,
+    "second" : 0.10
+#    "third" :
+}
+argument = sys.argv
+
+UsageMSG = """
+Usage for %s:
+
+main.py mm.dd.yyy number
+
+Mandatory arguments:
+<mm.dd.yyyy>                - Start date in MM.DD.YYYY format
+
+<number>                    - Number of days (1 to 365)
+
+
+""" % argument
+
+
+if len(argument) < 3:
+    print (UsageMSG)
+    exit()
+#if argument[2] > 365:
+#    print (UsageMSG)
+#    exit()
+
+date = argument[1]
+print (final_dates(date))
+
+print ("it was a " +  weekday[check_day(final_dates(date))])
